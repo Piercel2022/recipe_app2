17d9a9365c85aba6420d8d41aead2881b0cef9fa
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-07 23:55:55 +0100

    Add the workflow

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'foods#index'
+
+  resources :users, only: %i[index show]
+
+  resources :recipes, only: %i[index show new create destroy] do
+    resources :recipefoods, only: %i[index show]
+  end
+
+end
