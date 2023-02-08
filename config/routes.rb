30c15ded7b02a312aff1466018236a2813bab545
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-08 09:35:08 +0100

    Add the workflow

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,5 +11,4 @@ Rails.application.routes.draw do
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipefoods, only: %i[index show]
   end
-
 end
