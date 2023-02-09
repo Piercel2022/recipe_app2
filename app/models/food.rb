48dac3e2792fffb2c5a8758542ab67f21b24ad8e
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-09 10:59:14 +0100

    Add association in the Model food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,11 @@
 class Food < ApplicationRecord
+  belongs_to :user
+  has_many :recipefoods, dependent: :destroy
+
+  has_many :recipes, through: :recipefoods
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 250 }
+  validates :measurement_unit, presence: true, length: { maximum: 250 }
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
