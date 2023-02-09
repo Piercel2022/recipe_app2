8fda691216e6f7f6c9cba4fe2227d1c9b1f18b36
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-09 10:57:48 +0100

    Add the routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show]
 
   resources :recipes, only: %i[index show new create destroy] do
-    resources :recipefoods, only: %i[index show]
+    resources :recipefoods, only: %i[index show new create destroy]
+    resources :general_shopping_list, only: %i[index]
   end
+
+  resources :foods, only: %i[index show new create destroy] do
+  end
+
+  get 'public_recipes', to: 'recipes#public', as: 'public_recipes'
 end
