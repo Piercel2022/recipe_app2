e5763193ce37f4e1a34fd610a60f17298595abec
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-09 11:00:28 +0100

    Add association in the Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipefoods, dependent: :destroy
+
+  has_many :foods, through: :recipefoods
+
+  validates :name, presence: true, length: { minimus: 3, maximum: 250 }
 end
