a3c39282d93af28f61c94c9b1e464da2bebb7365
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-11 23:39:41 +0100

    Fix errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,7 +4,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
-
   # Validation
   validates :name, presence: true, length: { minimum: 3, maximum: 250 }
   validates :measurement_unit, presence: true, length: { maximum: 250 }
